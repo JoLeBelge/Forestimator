97b421ecdc5920253f983bd50544330fccb26d7e
lisein <liseinjon@hotmail.com>
2020-08-10 14:16:36 +0200

    tjrs dans portabilité tablette

--- cWebAptitude/wopenlayers.cpp
+++ cWebAptitude/wopenlayers.cpp
@@ -66,7 +66,7 @@ WOpenLayers::WOpenLayers(cDicoApt *aDico):xy_(this,"1.0"),mDico(aDico),polygId_(
                 "console.log(startX);"
                 "console.log(startY);"
                 + xy_.createCall({"startX","startY"}) +
-               "}"
+               "}}"
                 );
        //    "} else { console.log('no event');"
 
