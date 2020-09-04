34e9553cc52b78a9c7c40b23173ca123d2a5fb33
Chatmetaleux <samuel.quevauvillers@gmail.com>
2020-09-04 09:16:09 +0200

    update cmd .sh file

--- service/webaptitude.sh
+++ service/webaptitude.sh
@@ -2,5 +2,5 @@
 
 #pushd /data1/build-phytoTool
 #/data1/Forestimator/build-WebAptitude/WebAptitude --docroot /data1/Forestimator/build-WebAptitude --http-port 82 --http-addr 0.0.0.0
-/data1/Forestimator/build-WebAptitude/WebAptitude --deploy-path=/ --docroot "/data1/Forestimator/data/;/favicon.ico,/resources,/style,/tmp,/data,/js,/jslib" --http-port 82 --http-addr 0.0.0.0
+/data1/Forestimator/build-WebAptitude/WebAptitude --deploy-path=/ --docroot "/data1/Forestimator/data/;/favicon.ico,/resources,/style,/tmp,/data,/js,/jslib" --http-port 82 --http-addr 0.0.0.0 -c /data1/Forestimator/data/wt_config.xml
 #popd
