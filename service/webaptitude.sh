af6e003344d8ddf5b4f623460ae610888ce62c3c
root <root@gx-cloud31.segi.ulg.ac.be>
2021-02-23 10:53:27 +0100

    fix referencing

--- service/webaptitude.sh
+++ service/webaptitude.sh
@@ -2,5 +2,5 @@
 
 #pushd /data1/build-phytoTool
 #/data1/Forestimator/build-WebAptitude/WebAptitude --docroot /data1/Forestimator/build-WebAptitude --http-port 82 --http-addr 0.0.0.0
-/data1/Forestimator/build-WebAptitude/WebAptitude --deploy-path=/ --docroot "/data1/Forestimator/data/;/favicon.ico,/resources,/style,/tmp,/data,/js,/jslib" --http-port 82 --http-addr 0.0.0.0 -c /data1/Forestimator/data/wt_config.xml
+/data1/Forestimator/build-WebAptitude/WebAptitude --deploy-path=/ --docroot "/data1/Forestimator/data/;/favicon.ico,/google52ee6b8ebe0b4b19.html,/sitemap.xml,/resources,/style,/tmp,/data,/js,/jslib,/img" --http-port 82 --http-addr 0.0.0.0 -c /data1/Forestimator/data/wt_config.xml
 #popd
