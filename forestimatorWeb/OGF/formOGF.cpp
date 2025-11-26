c4a5e59af1e19a500eafa18201a56ca25a462f67
lisein <liseinjon@hotmail.com>
2025-11-26 16:26:35 +0100

    staticMap may create app crash

--- forestimatorWeb/OGF/formOGF.cpp
+++ forestimatorWeb/OGF/formOGF.cpp
@@ -511,15 +511,15 @@ void formOGF::sendSummaryMail()
     mail.setSubject(Wt::WString::tr("mail.titre").toUTF8());
     mail.addRecipient(Wt::Mail::RecipientType::To, Mail::Mailbox(contactEncoderEdit_->valueText().toUTF8(), nomEncoderEdit_->valueText().toUTF8()));
 
-    // création de la figure de localisation
-    staticMap sm(mDico->getLayerBase("IGN"), geom);
-    std::ifstream in;
-    in.open(sm.getFileName(), std::ios::in);
-    mail.addAttachment("image/png", "localisationOGF.png", &in);
+    // création de la figure de localisation - j'enlève
+    //staticMap sm(mDico->getLayerBase("IGN"), geom); // ERROR 1: GDALWMS: Unable to download block 0, 0.... Dbo.Session: Session exiting with 1 dirty objects
+    //std::ifstream in;
+    //in.open(sm.getFileName(), std::ios::in);
+    //mail.addAttachment("image/png", "localisationOGF.png", &in);
     Mail::Client client;
     client.connect();
     client.send(mail);
-    in.close(); // après l'envoi!! car le addAttachement pointe ver le ifstream!
+    //in.close(); // après l'envoi!! car le addAttachement pointe ver le ifstream!
     mail = Wt::Mail::Message();
     mail.addRecipient(Wt::Mail::RecipientType::To, Mail::Mailbox("liseinjon@hotmail.com", "Lisein Jonathan"));
     mail.setFrom(Wt::Mail::Mailbox("JO.Lisein@uliege.be", "Lisein Jonathan"));
